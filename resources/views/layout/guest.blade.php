7523bf050e466f6d6344be82c1c15b0c18b06b3a
abeliooo <stefanusabel29@gmail.com>
2025-09-29 17:09:28 +0700

    slightly typo
    :D

--- resources/views/layout/guest.blade.php
+++ resources/views/layout/guest.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{ config('app.name', 'Adnmin') }}</title>
+    <title>{{ config('app.name', 'Admin') }}</title>
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 </head>
